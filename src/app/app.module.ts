3c133d5b0e968e13b2a129da73f4fa1ea4e4e7b4
v.prots <vitalikprocj@gmail.com>
2018-12-11 23:38:30 +0200

    fix style, add new component grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { GridComponent } from './grid/grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
