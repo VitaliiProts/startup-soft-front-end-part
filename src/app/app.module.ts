997e8826ba10dc378f5b0aeaf3dd6054e7034b71
v.prots <vitalikprocj@gmail.com>
2018-12-12 03:59:26 +0200

    add modal window, search items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { GridComponent } from './grid/grid.component';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    GridComponent
+    GridComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,13 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.scss']
 })
-export class GridComponent implements OnInit {
 
-  constructor() { }
+export class GridComponent implements OnInit {
+  closeResult: string;
+  items = [
+    {
+      name: 'skadfhksdhkf',
+      img: 'http://fakeimg.pl/325x325/',
+      price: '$ 300',
+      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, itaque.'
+    },
+    {
+      name: 'kdfklsdgklflg',
+      img: 'http://fakeimg.pl/325x325/',
+      price: '$ 200',
+      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, itaque.'
+    },
+    {
+      name: 'knsl;dgj;ld3',
+      img: 'http://fakeimg.pl/325x325/',
+      price: '$ 370',
+      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, itaque.'
+    },
+    {
+      name: 'ksjhgur',
+      img: 'http://fakeimg.pl/325x325/',
+      price: '$ 140',
+      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, itaque.'
+    },
+    {
+      name: 'rsgjkldg',
+      img: 'http://fakeimg.pl/325x325/',
+      price: '$ 356',
+      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, itaque.'
+    },
+    {
+      name: 'slkgjlkjhkl',
+      img: 'http://fakeimg.pl/325x325/',
+      price: '$ 450',
+      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, itaque.'
+    },
+  ]
+  constructor(private modalService: NgbModal) { }
+
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 
   ngOnInit() {
   }
